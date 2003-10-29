39dc24a96d09d54d66cf9fc9784f06a536b583c5
Shane Caraveo <shane@php.net>
2003-10-29 05:48:16 +0000

    php 4 soap files moved to ZEND_ENGINE_1 branch removing files from HEAD, new soap module is ground up rewrite based on php 5 and libxml/DOM
    git-svn-id: http://svn.php.net/repository/pear/packages/SOAP/trunk@143315 c90b9560-bf6c-de11-be94-00142212c4b1

--- test/all.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'SOAP/Base.php';
-$GLOBALS['SOAP_OBJECT_STRUCT'] = FALSE;
-
-require_once('5.2.simpletypes.php');
-require_once('5.2.1.simpletypes.string.php');
-require_once('5.2.1.simpletypes.multiref.php');
-?>
