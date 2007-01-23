ec87f271b25d8e36f97ebe200bbf964d632b640d
Jan Schneider <yunosh@php.net>
2007-01-23 16:51:59 +0000

    CS
    git-svn-id: http://svn.php.net/repository/pear/packages/SOAP/trunk@227972 c90b9560-bf6c-de11-be94-00142212c4b1

--- Type/hexBinary.php
+++ Type/hexBinary.php
@@ -1,46 +1,46 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Shane Caraveo <Shane@Caraveo.com>   Port to PEAR and more   |
-// | Authors: Dietrich Ayala <dietrich@ganx4.com> Original Author         |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
-class SOAP_Type_hexBinary
-{
+/**
+ * This class provides methods to detect and convert binary data from an to
+ * hexadecimal strings.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 2.02 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is available at
+ * through the world-wide-web at http://www.php.net/license/2_02.txt.  If you
+ * did not receive a copy of the PHP license and are unable to obtain it
+ * through the world-wide-web, please send a note to license@php.net so we can
+ * mail you a copy immediately.
+ *
+ * @category   Web Services
+ * @package    SOAP
+ * @author     Dietrich Ayala <dietrich@ganx4.com> Original Author
+ * @author     Shane Caraveo <Shane@Caraveo.com>   Port to PEAR and more
+ * @copyright  2003-2007 The PHP Group
+ * @license    http://www.php.net/license/2_02.txt  PHP License 2.02
+ * @link       http://pear.php.net/package/SOAP
+ */
+class SOAP_Type_hexBinary {
+
     function to_bin($value)
     {
-        $len = strlen($value);
-        return pack('H' . $len, $value);
+        return pack('H' . strlen($value), $value);
     }
+
     function to_hex($value)
     {
         return bin2hex($value);
     }
+
     function is_hexbin($value)
     {
-        # first see if there are any invalid chars
+        // First see if there are any invalid chars.
         $l = strlen($value);
+        if (!$l || strspn($value, '0123456789ABCDEFabcdef') != $l) {
+            return false;
+        }
 
-        if ($l < 1 || strspn($value, '0123456789ABCDEFabcdef') != $l) return FALSE;
-
-        $bin = SOAP_Type_hexBinary::to_bin($value);
-        $hex = SOAP_Type_hexBinary::to_hex($bin);
-        return strcasecmp($value, $hex) == 0;
+        return strcasecmp($value, SOAP_Type_hexBinary::to_hex(SOAP_Type_hexBinary::to_bin($value))) == 0;
     }
-}
 
-?>
+}
