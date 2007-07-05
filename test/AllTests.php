77c157c0c0469cf39bae3004388705be428a8965
Christian Weiske <cweiske@php.net>
2007-07-05 06:42:05 +0000

    We're not phpunit
    git-svn-id: http://svn.php.net/repository/pear/packages/SOAP/trunk@239175 c90b9560-bf6c-de11-be94-00142212c4b1

--- test/AllTests.php
+++ test/AllTests.php
@@ -3,9 +3,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'SOAP_AllTests::main');
 }
 
-if (!defined('PHPUnit_INSIDE_OWN_TESTSUITE')) {
-    define('PHPUnit_INSIDE_OWN_TESTSUITE', TRUE);
-}
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
